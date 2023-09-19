cae6f84c83014c7be6999e3ae9482a52da614988
Vasko <vvassilev515@gmail.com>
2023-09-19 11:20:23 -0600

    Transferred comments to Spreadsheet.cs

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -13,37 +13,94 @@ namespace SS
 
         }
 
-
+        /// <summary>
+        /// If name is invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns the contents (as opposed to the value) of the named cell.
+        /// The return value should be either a string, a double, or a Formula.
+        /// </summary>
         public override object GetCellContents(string name)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Enumerates the names of all the non-empty cells in the spreadsheet.
+        /// </summary>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// If name is invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes number.  The method returns a
+        /// list consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// list {A1, B1, C1} is returned.
+        /// </summary>
         public override IList<string> SetCellContents(string name, double number)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// If name is invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes text.  The method returns a
+        /// list consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// list {A1, B1, C1} is returned.
+        /// </summary>
         public override IList<string> SetCellContents(string name, string text)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// If name is invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
+        /// circular dependency, throws a CircularException, and no change is made to the spreadsheet.
+        /// 
+        /// Otherwise, the contents of the named cell becomes formula.  The method returns a
+        /// list consisting of name plus the names of all other cells whose value depends,
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// list {A1, B1, C1} is returned.
+        /// </summary>
         public override IList<string> SetCellContents(string name, Formula formula)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Returns an enumeration, without duplicates, of the names of all cells whose
+        /// values depend directly on the value of the named cell.  In other words, returns
+        /// an enumeration, without duplicates, of the names of all cells that contain
+        /// formulas containing name.
+        /// 
+        /// For example, suppose that
+        /// A1 contains 3
+        /// B1 contains the formula A1 * A1
+        /// C1 contains the formula B1 + A1
+        /// D1 contains the formula B1 - C1
+        /// The direct dependents of A1 are B1 and C1
+        /// </summary>
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Cell class for private use only, representation of a single cell in excel 
+        /// </summary>
         private class Cell
         {
             private object contents; // contents of the cell
