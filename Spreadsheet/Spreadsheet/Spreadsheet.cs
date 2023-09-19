47b1e91583a0c50e9d032a4d2e7edbf040c0431b
Vasko <vvassilev515@gmail.com>
2023-09-19 13:15:38 -0600

    All methods that don't deal with dependencies are complete, unsure of how dependencies should be managed

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -1,16 +1,63 @@
 ﻿using SpreadsheetUtilities;
+using System.Text.RegularExpressions;
 
 namespace SS
 {
+    /// <summary>
+    /// An AbstractSpreadsheet object represents the state of a simple spreadsheet.  A 
+    /// spreadsheet consists of an infinite number of named cells.
+    /// 
+    /// A string is a valid cell name if and only if:
+    ///   (1) its first character is an underscore or a letter
+    ///   (2) its remaining characters (if any) are underscores and/or letters and/or digits
+    /// Note that this is the same as the definition of valid variable from the PS3 Formula class.
+    /// 
+    /// For example, "x", "_", "x2", "y_15", and "___" are all valid cell  names, but
+    /// "25", "2x", and "&" are not.  Cell names are case sensitive, so "x" and "X" are
+    /// different cell names.
+    /// 
+    /// A spreadsheet contains a cell corresponding to every possible cell name.  (This
+    /// means that a spreadsheet contains an infinite number of cells.)  In addition to 
+    /// a name, each cell has a contents and a value.  The distinction is important.
+    /// 
+    /// The contents of a cell can be (1) a string, (2) a double, or (3) a Formula.  If the
+    /// contents is an empty string, we say that the cell is empty.  (By analogy, the contents
+    /// of a cell in Excel is what is displayed on the editing line when the cell is selected).
+    /// 
+    /// In a new spreadsheet, the contents of every cell is the empty string.
+    ///  
+    /// We are not concerned with values in PS4, but to give context for the future of the project,
+    /// the value of a cell can be (1) a string, (2) a double, or (3) a FormulaError.  
+    /// (By analogy, the value of an Excel cell is what is displayed in that cell's position
+    /// in the grid). 
+    /// 
+    /// If a cell's contents is a string, its value is that string.
+    /// 
+    /// If a cell's contents is a double, its value is that double.
+    /// 
+    /// If a cell's contents is a Formula, its value is either a double or a FormulaError,
+    /// as reported by the Evaluate method of the Formula class.  The value of a Formula,
+    /// of course, can depend on the values of variables.  The value of a variable is the 
+    /// value of the spreadsheet cell it names (if that cell's value is a double) or 
+    /// is undefined (otherwise).
+    /// 
+    /// Spreadsheets are never allowed to contain a combination of Formulas that establish
+    /// a circular dependency.  A circular dependency exists when a cell depends on itself.
+    /// For example, suppose that A1 contains B1*2, B1 contains C1*2, and C1 contains A1*2.
+    /// A1 depends on B1, which depends on C1, which depends on A1.  That's a circular
+    /// dependency.
+    /// </summary>
     public class Spreadsheet : AbstractSpreadsheet
     {
-
+        private Dictionary<string, Cell> cells;
+        private DependencyGraph dependencyGraph;
         /// <summary>
         /// Zero argument constructor for making a blank spreadsheet
         /// </summary>
         public Spreadsheet()
         {
-
+            cells = new Dictionary<string, Cell>();
+            dependencyGraph = new DependencyGraph();
         }
 
         /// <summary>
@@ -21,7 +68,28 @@ namespace SS
         /// </summary>
         public override object GetCellContents(string name)
         {
-            throw new NotImplementedException();
+            if (IsValidName(name) && cells.ContainsKey(name))
+            {
+                return cells[name].contents;
+            }
+            else if (IsValidName(name))
+            {
+                return "";
+            }
+            else
+            {
+                throw new InvalidNameException();
+            }
+        }
+
+        /// <summary>
+        /// Helper method do decide if the input string is a valid variable
+        /// </summary>
+        /// <param name="str">input string that is being validated for proper variable format</param> 
+        /// <returns>boolean whether the string is a valid variable format</returns> 
+        private static bool IsValidName(string str)
+        {
+            return Regex.IsMatch(str, @"^[a-zA-Z_][a-zA-Z0-9_]*$");
         }
 
         /// <summary>
@@ -29,7 +97,19 @@ namespace SS
         /// </summary>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
-            throw new NotImplementedException();
+            List<string> nonEmptyCellNames = new List<string>();
+
+            // for every cell that has been "added/modified from base state"
+            // if not null or empty add it
+            foreach (string cellName in cells.Keys)
+            {
+                if (!string.IsNullOrEmpty(cells[cellName].contents.ToString()))
+                {
+                    nonEmptyCellNames.Add(cellName);
+                }
+            }
+            return nonEmptyCellNames;
+
         }
 
         /// <summary>
@@ -103,7 +183,7 @@ namespace SS
         /// </summary>
         private class Cell
         {
-            private object contents; // contents of the cell
+            public object contents { get; set; } // contents of the cell
             private Func<string, double> lookup;
 
             public Cell()
@@ -126,6 +206,7 @@ namespace SS
                 contents = formula;
                 lookup = _lookup;
             }
+
         }
     }
 }
