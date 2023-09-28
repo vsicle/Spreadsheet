a0cc8a8c132f62a43d9a8151fd9ecf65be68f833
Vasko <vvassilev515@gmail.com>
2023-09-27 22:29:02 -0600

    LAb6

---
+++ Lab6/Lab6/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Lab6
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Lab6/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        double input;
+        double tipInput;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            TipAmount.Text = (input * (tipInput / 100)).ToString();
+            TotalAmount.Text = (input * (tipInput / 100) + input).ToString();
+            SemanticScreenReader.Announce(FindTipBtn.Text);
+        }
+
+        private void OnBillAmountChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(BillAmount.Text, out _))
+            {
+                FindTipBtn.IsEnabled = true;
+                input = double.Parse(BillAmount.Text);
+            }
+            else
+            {
+                FindTipBtn.IsEnabled = false;
+            }
+            
+        }
+
+        private void OnTipInputChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(TipInput.Text, out _))
+            {
+                FindTipBtn.IsEnabled = true;
+                tipInput = double.Parse(TipInput.Text);
+            }
+            else
+            {
+                FindTipBtn.IsEnabled = false;
+            }
+            
+        }
+    }
+}
